dd3943062476ee42b72e21338d806759d526c18f
Aharon laptop <ahrn321@gmail.com>
2022-07-04 23:26:05 +0300

    added major functions & data

--- include/spi.h
+++ include/spi.h
@@ -11,6 +11,7 @@
 #define True 1
 #define False 0
 
+
 /*
  * define basic pinout diagram.
  */
@@ -19,12 +20,207 @@
 // The spi pins:
 #define SPI_DIR P1DIR
 #define SPI_OUT P1OUT
+#define SPI_IN  P1IN
 #define SS_SD BIT5
 #define SS_WIFI BIT6
-#define SLAVEREADY BIT10
+#define SLAVEREADY BITA
 #define DATAIN BIT1
 #define DATAOUT BIT2
 #define SPICLOCK BIT4
+
+/*
+ * define data communication values.
+ */
+#define DUMMY 0xff
+#define START_CMD 0xE0
+#define END_CMD 0xEE
+#define ERR_CMD 0xEF
+#define CMD_FLAG 0x0 
+#define REPLY_FLAG 1<<7
+#define DATA_FLAG 0x40 
+#define WIFI_SPI_ACK 0x01
+#define WIFI_SPI_ERR 0xFF
+#define TIMEROUT_CHAR 1000
+#define MAX_SOCK 0xff
+#define SPI_START_CMD_DELAY 12
+#define DELAY_SUB 3
+
+/*
+ * define wifi 
+ */
+
+// Maximum size of a SSID
+#define WL_SSID_MAX_LENGTH 32
+// Length of passphrase. Valid lengths are 8-63.
+#define WL_WPA_KEY_MAX_LENGTH 63
+// Length of key in bytes. Valid values are 5 and 13.
+#define WL_WEP_KEY_MAX_LENGTH 13
+// Size of a MAC-address or BSSID
+#define WL_MAC_ADDR_LENGTH 6
+// Size of a MAC-address or BSSID
+#define WL_IPV4_LENGTH 4
+// Maximum size of a SSID list
+#define WL_NETWORKS_LIST_MAXNUM	10
+// Maxmium number of socket
+#define	MAX_SOCK_NUM		4
+//Maximum number of attempts to establish wifi connection
+#define WL_MAX_ATTEMPT_CONNECTION	10
+
+
+
+typedef enum {
+		WL_NO_SHIELD = 255,
+        WL_IDLE_STATUS = 0,
+        WL_NO_SSID_AVAIL,
+        WL_SCAN_COMPLETED,
+        WL_CONNECTED,
+        WL_CONNECT_FAILED,
+        WL_CONNECTION_LOST,
+        WL_DISCONNECTED
+} wl_status_t;
+
+/* Encryption modes */
+enum wl_enc_type {  /* Values map to 802.11 encryption suites... */
+        ENC_TYPE_WEP  = 5,
+        ENC_TYPE_TKIP = 2,
+        ENC_TYPE_CCMP = 4,
+        /* ... except these two, 7 and 8 are reserved in 802.11-2007 */
+        ENC_TYPE_NONE = 7,
+        ENC_TYPE_AUTO = 8
+};
+
+
+
+/*
+ * define 
+ */
+
+enum {
+	SET_NET_CMD 		= 0x10,
+	SET_PASSPHRASE_CMD	= 0x11,
+	SET_KEY_CMD	        = 0x12,
+	TEST_CMD	        = 0x13,
+
+	GET_CONN_STATUS_CMD	= 0x20,
+	GET_IPADDR_CMD		= 0x21,
+	GET_MACADDR_CMD		= 0x22,
+	GET_CURR_SSID_CMD	= 0x23,
+	GET_CURR_BSSID_CMD	= 0x24,
+	GET_CURR_RSSI_CMD	= 0x25,
+	GET_CURR_ENCT_CMD	= 0x26,
+	SCAN_NETWORKS		= 0x27,
+	START_SERVER_TCP_CMD= 0x28,
+	GET_STATE_TCP_CMD   = 0x29,
+	DATA_SENT_TCP_CMD	= 0x2A,
+    AVAIL_DATA_TCP_CMD	= 0x2B,
+    GET_DATA_TCP_CMD	= 0x2C,
+    START_CLIENT_TCP_CMD= 0x2D,
+    STOP_CLIENT_TCP_CMD = 0x2E,
+    GET_CLIENT_STATE_TCP_CMD= 0x2F,
+    DISCONNECT_CMD		= 0x30,
+	GET_IDX_SSID_CMD	= 0x31,
+	GET_IDX_RSSI_CMD	= 0x32,
+	GET_IDX_ENCT_CMD	= 0x33,
+	REQ_HOST_BY_NAME_CMD= 0x34,
+	GET_HOST_BY_NAME_CMD= 0x35,
+	START_SCAN_NETWORKS	= 0x36,
+	GET_FW_VERSION_CMD	= 0x37,
+
+    // All command with DATA_FLAG 0x40 send a 16bit Len
+
+	SEND_DATA_TCP_CMD		= 0x44,
+    GET_DATABUF_TCP_CMD		= 0x45,
+};
+
+
+enum wl_tcp_state {
+  CLOSED      = 0,
+  LISTEN      = 1,
+  SYN_SENT    = 2,
+  SYN_RCVD    = 3,
+  ESTABLISHED = 4,
+  FIN_WAIT_1  = 5,
+  FIN_WAIT_2  = 6,
+  CLOSE_WAIT  = 7,
+  CLOSING     = 8,
+  LAST_ACK    = 9,
+  TIME_WAIT   = 10
+};
+
+
+enum numParams{
+    PARAM_NUMS_0,
+    PARAM_NUMS_1,
+    PARAM_NUMS_2,
+    PARAM_NUMS_3,
+    PARAM_NUMS_4,
+    PARAM_NUMS_5,
+    MAX_PARAM_NUMS
+};
+
+#define MAX_PARAMS MAX_PARAM_NUMS-1
+#define PARAM_LEN_SIZE 1
+
+typedef struct  __attribute__((__packed__))
+{
+	uint8_t     paramLen;
+	char*	    param;
+}tParam;
+
+typedef struct  __attribute__((__packed__))
+{
+	uint16_t     dataLen;
+	char*	     data;
+}tDataParam;
+
+
+typedef struct  __attribute__((__packed__))
+{
+	unsigned char	cmd;
+	unsigned char	tcmd;
+	unsigned char	nParam;
+	tParam	params[MAX_PARAMS];
+}tSpiMsg;
+
+typedef struct  __attribute__((__packed__))
+{
+	unsigned char	cmd;
+	unsigned char	tcmd;
+	unsigned char	nParam;
+	tDataParam		params[MAX_PARAMS];
+}tSpiMsgData;
+
+
+typedef struct  __attribute__((__packed__))
+{
+	unsigned char	cmd;
+	unsigned char	tcmd;
+	//unsigned char	totLen;
+	unsigned char	nParam;
+}tSpiHdr;
+
+typedef struct  __attribute__((__packed__))
+{
+	uint8_t     paramLen;
+	uint32_t	param;
+}tLongParam;
+
+typedef struct  __attribute__((__packed__))
+{
+	uint8_t     paramLen;
+	uint16_t	param;
+}tIntParam;
+
+typedef struct  __attribute__((__packed__))
+{
+	uint8_t     paramLen;
+	uint8_t	param;
+}tByteParam;
+
+
+
+#define send(data) sndArcv(data)
+#define sendW(data) sndArcvW(data)
 
 
 /*
@@ -49,6 +245,22 @@ void Wifi_spi_begin();
  */
 void deselect_sd();
 void deselect_wifi();
+
+
+int getSlaveSign();
+
+void waitForSlaveSign();
+void waitForSlaveReady();
+
+void delay(unsigned int);
+
+void sendCmd(uint8_t, uint8_t);
+void sendParam(uint16_t, uint16_t);
+void sendParamW(uint16_t);
+void sendParamC(uint8_t);
+void sendBuffer(uint8_t*, uint16_t, uint8_t);
+uint16_t readParamW(uint16_t*);
+uint8_t readParamC(uint8_t*);
 
 
 #endif
--- src/spi.c
+++ src/spi.c
@@ -35,12 +35,104 @@ void spi_initate()
     // SPI config
 }
 
-uint8_t sndArcv(uint8_t data)
+uint8_t sndArcv(volatile register uint8_t data)
 {
+    /*
+     * need to delay for read the result buffer...
+     */
 
 }
 
-uint16_t sndArcvW(uint16_t data)
+uint16_t sndArcvW(volatile register uint16_t data)
 {
+    uint8_t data1, data2;
+    data1 = (uint8_t) (data & 0xff00) >> 8;
+    data2 = (uint8_t) data & 0xff;
+    data1 = sndArcv(data1);
+    data2 = sndArcv(data2);
+    data = ((data1 << 8) & 0xff00) | data2;
+    return data;
+}
+
+int getSlaveSign()
+{
+    return SPI_IN & SLAVEREADY;
+}
+
+void waitForSlaveSign()
+{
+    while (!getSlaveSign());
+}
+
+void waitForSlaveReady()
+{
+    while (getSlaveSign());
+}
+
+void delay(unsigned int time)
+{
+    while (time) time--;
+}
+
+
+void sendCmd(uint8_t cmd, uint8_t numParam)
+{
+    send(START_CMD);
+
+    delay(SPI_START_CMD_DELAY - DELAY_SUB);
+
+    send(cmd & ~(REPLY_FLAG));
+
+    send(numParam);
+
+    if (!numParam)
+        send(END_CMD);
 
 }
+
+void sendParamW(uint16_t paramLen)
+{
+    sendW(paramLen);
+}
+
+void sendParamC(uint8_t paramLen)
+{
+    send(paramLen);
+}
+
+void sendParam(uint16_t param, uint16_t lastParam)
+{
+    sendParamC(2); // param len
+    
+    sendW(param);
+
+    if (lastParam == 1)
+        send(END_CMD);
+}
+
+uint16_t readParamW(uint16_t* len)
+{
+    uint16_t _param_len = send(DUMMY)<<8 | (send(DUMMY)&0xff);
+    if (len != NULL)
+        *len = _param_len;
+
+    return _param_len;
+}
+
+uint8_t readParamC(uint8_t* len)
+{
+    uint8_t param_len = send(DUMMY);
+    if (len != NULL)
+        *len = param_len;
+    return param_len;
+}
+void sendBuffer(uint8_t* param, uint16_t param_len, uint8_t lastParam)
+{
+    uint16_t i = 0;
+    sendParamW(param_len);
+    for (; i < param_len; ++i)
+        send(param[i]);
+    if (lastParam == 1)
+        send(END_CMD);
+}
+
